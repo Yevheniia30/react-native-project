2d4161201739355a098e65d679f26e507cc50ef9
Yevheniia Suska <susskaya86@gmail.com>
2022-01-23 14:14:34 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
-export default function App() {
+export default App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello React Native World!</Text>
+      <TextInput placeholder="Enter message" style={styles.input} />
+      <Button title="Send"></Button>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderBottomWidth: 1,
+
+    padding: 10,
+    // borderBottomColor: "#000",
   },
 });
